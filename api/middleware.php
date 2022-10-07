51ef5a8a508b878070433c9a3713516e5f1f1095
cristianbota001 <c.bota@ifortech.com>
2022-10-07 13:59:09 +0200

    inint

---
+++ api/middleware.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once("./logic.php");
+
+class Middleware{
+    
+    function __construct($request){
+        $this->request = $request;
+        $this->logic = new Logic();
+        $this->SwitchRequestMethod();
+    }
+
+    private function SwitchRequestMethod(){
+        switch ($this->request["REQUEST_METHOD"]){
+            case "GET":{
+                $this->SwitchMethodGet();
+            } break;
+            case "POST":{
+                $this->SwitchMethodPost();
+            } break;
+            case "PUT":{
+                $this->SwitchMethodPut();
+            } break;
+            case "DELETE":{
+                $this->SwitchMethodDelete();
+            } break;
+        }
+    }
+
+    private function SwitchMethodGet(){
+        
+    }
+
+    private function SwitchMethodPost(){
+       
+    }
+
+    private function SwitchMethodPut(){
+        
+    }
+
+    private function SwitchMethodDelete(){
+        
+    }
+}
+
